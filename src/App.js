52b3ec22d6fb521a9728f29b514639c41825e17e
EnzoFerreira <enzoraci21@gmail.com>
2023-01-21 11:28:03 -0300

    healthy restart

--- src/App.js
+++ src/App.js
@@ -1,74 +1,68 @@
-import { useState } from "react";
-import Banner from "./components/Banner/Banner.jsx";
-import Form from "./components/form/Form.jsx";
-import Time from "./components/Time/Time.jsx";
+import { useState } from 'react';
+import Banner from './componentes/Banner';
+import Formulario from './componentes/Formulario';
+import Time from './componentes/Time';
 
 function App() {
+
   const times = [
     {
-      nome: "programação",
-      corPrimaria: "#57C278",
-      corSecundaria: "#D9F7E9",
+      nome: 'Programação',
+      corPrimaria: '#57C278',
+      corSecundaria: '#D9F7E9'
     },
     {
-      nome: "front end",
-      corPrimaria: "#82CFFA",
-      corSecundaria: "#E8F8FF",
+      nome: 'Front-End',
+      corPrimaria: '#82CFFA',
+      corSecundaria: '#E8F8FF'
     },
     {
-      nome: "data science",
-      corPrimaria: "#A6D157",
-      corSecundaria: "#F0F8E2",
+      nome: 'Data Science',
+      corPrimaria: '#A6D157',
+      corSecundaria: '#F0F8E2'
     },
     {
-      nome: "dev ops",
-      corPrimaria: "#E06B69",
-      corSecundaria: "#FDE7E8",
+      nome: 'Devops',
+      corPrimaria: '#E06B69',
+      corSecundaria: '#FDE7E8'
     },
     {
-      nome: "mobile",
-      corPrimaria: "#FFBA05",
-      corSecundaria: "#FFF5D9",
+      nome: 'UX e Design',
+      corPrimaria: '#DB6EBF',
+      corSecundaria: '#FAE9F5'
     },
     {
-      nome: "inovação e gestao",
-      corPrimaria: "#FF8A29",
-      corSecundaria: "#FFEEDF",
+      nome: 'Mobile',
+      corPrimaria: '#FFBA05',
+      corSecundaria: '#FFF5D9'
     },
     {
-      nome: "UX design",
-      corPrimaria: "#D86EBF",
-      corSecundaria: "#FAE9F5",
-    },
-  ];
+      nome: 'Inovação e Gestão',
+      corPrimaria: '#FF8A29',
+      corSecundaria: '#FFEEDF'
+    }
+  ]
 
-  const [colaboradores, setColaboradores] = useState([]);
+  const [colaboradores, setColaboradores] = useState([])
 
   const aoNovoColaboradorAdicionado = (colaborador) => {
-    console.log(colaborador);
-    setColaboradores([...colaboradores, colaborador]);
-  };
+    debugger
+    setColaboradores([...colaboradores, colaborador])
+  }
 
   return (
     <div className="App">
       <Banner />
-      <Form
-        times={times.map((times) => times.nome)}
-        aoColaboradorCadastrado={(colaborador) =>
-          aoNovoColaboradorAdicionado(colaborador)
-        }
-      />
-      {times.map((time) => (
-        <Time
-          key={time.nome}
-          nome={time.nome}
-          corPrimaria={time.corPrimaria}
-          corSecundaria={time.corSecundaria}
-          colaboradores={colaboradores.filter(
-            (colaborador) => colaborador.time === time.nome
-          )}
-        />
-      ))}
+      <Formulario times={times.map(time => time.nome)} aoColaboradorCadastrado={colaborador => aoNovoColaboradorAdicionado(colaborador)}/>
+
+      {times.map(time => <Time 
+        key={time.nome} 
+        nome={time.nome} 
+        corPrimaria={time.corPrimaria} 
+        corSecundaria={time.corSecundaria} 
+        colaboradores={colaboradores.filter(colaborador => colaborador.time === time.nome)}
+      />)}   
+
     </div>
   );
 }
